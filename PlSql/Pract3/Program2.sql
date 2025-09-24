b52f38945bf12de7a511ca046aaee56d9da2d6a2
Vedant Deshmukh <73630434+Vedant9500@users.noreply.github.com>
2025-09-24 14:39:49 +0530

    Refactor PL/SQL program to process all students

--- PlSql/Pract3/Program2.sql
+++ PlSql/Pract3/Program2.sql
@@ -1,14 +1,11 @@
 declare
-v_r number;
-total number;
-nm varchar2(50);
-gr varchar2(20);
-begin
-v_r := &r;
-select roll_no, name, total_mark into v_r, nm, total from student_mark where roll_no = v_r;
-proc_grade(v_r, total, gr);
-DBMS_OUTPUT.PUT_LINE(v_r || ' ' || nm || ' ' || gr);
-insert into student_result(roll_no, name, grade) values (v_r, nm, gr);
-DBMS_OUTPUT.PUT_LINE('one record inserted');
-end;
+gr VARCHAR2(20);
+brgin
+FOR rec IN (SELECT roll_no, name, total_mark FROM student_mark) LOOP
+proc_grade(rec.roll_no, rec.total_mark, gr);
+DBMS_OUTPUT.PUT_LINE(rec.roll_no || ' ' || rec.name || ' ' || gr);
+insert into student_result(roll_no, name, grade) values (rec.roll_no, rec.name, gr);
+DBMS_OUTPUT.PUT_LINE('One record inserted');
+END LOOP;
+END;
 /
