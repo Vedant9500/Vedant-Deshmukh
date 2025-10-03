dde202d033a24d37f7edae897f24511c835ea0eb
Vedant Deshmukh <73630434+Vedant9500@users.noreply.github.com>
2025-10-03 10:06:16 +0530

    Adjust price update logic in update.sql

--- PlSql/Pract4/update.sql
+++ PlSql/Pract4/update.sql
@@ -1,7 +1,7 @@
 DECLARE
-v_increase NUMBER := 2.10;  
+v_increase NUMBER := 1.5;  
 BEGIN
-FOR rec IN (SELECT pid, price FROM product WHERE price < 5000) LOOP
+FOR rec IN (SELECT pid, price FROM product WHERE price < 10000) LOOP
 UPDATE product
 SET price = rec.price * v_increase
 WHERE pid = rec.pid;
@@ -12,3 +12,4 @@ COMMIT;
 DBMS_OUTPUT.PUT_LINE('Prices updated.');
 END;
 /
+
